bc2620fc5ae75b1407c102516eb8fc649eea7644
Justin Blaisdell <jsblaisdell@gmail.com>
2013-09-06 10:12:32 -0500

    Fill out add_book method so that it works. Also added to_s method to check what books have been added to the library.

--- library.rb
+++ library.rb
@@ -1,11 +1,15 @@
 class Library
 
   @@books = {}
+  def to_s
+    puts "#{@@books}"
+  end
 
   # Add a book to the library.
   #
   # book - An instance of the Book class.
   def add_book(book)
+    @@books[book.title] = book
   end
 
   # Check out a book.
@@ -16,6 +20,7 @@ class Library
 end
 
 class Book
+  attr_reader :author, :title, :description
   def initialize(author, title, description)
     @author = author
     @title = title
@@ -31,6 +36,12 @@ end
 # For testing
 
 lib = Library.new
-book_1 = Book.new
-book_2 = Book.new
-book_3 = Book.new
+book_1 = Book.new("First Author", "Ruby Book 1", "Description of book 1")
+book_2 = Book.new("Second Author", "Ruby Book 2", "Description of book 2")
+book_3 = Book.new("Third Author", "Ruby Book 3", "Description of book 3")
+
+lib.add_book(book_1)
+lib.add_book(book_2)
+lib.add_book(book_3)
+
+lib.to_s
