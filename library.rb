e0ed8a65f32b0fe5ba422127988a756c67e24342
Justin Blaisdell <jsblaisdell@gmail.com>
2013-09-06 10:40:54 -0500

    Users can not check out books

--- library.rb
+++ library.rb
@@ -1,6 +1,7 @@
 class Library
 
   @@books = {}
+  # See the books currently in the library.
   def to_s
     puts "#{@@books}"
   end
@@ -15,7 +16,9 @@ class Library
   # Check out a book.
   #
   # book - An instance of the Book class from @@books.
-  def check_out(book)
+  # user - An instance of the User class.
+  def check_out(book, user)
+    user.check_out(@@books[book.title])
   end
 end
 
@@ -30,18 +33,35 @@ end
 
 class User
   @@checked_out_books = {}
+  def to_s
+    puts "#{@@checked_out_books}"
+  end
+  def check_out(book)
+    @@checked_out_books[book.title] = book
+  end
 end
 
 # ====================================================================
 # For testing
 
+# Create library
 lib = Library.new
+
+# Create books
 book_1 = Book.new("First Author", "Ruby Book 1", "Description of book 1")
 book_2 = Book.new("Second Author", "Ruby Book 2", "Description of book 2")
 book_3 = Book.new("Third Author", "Ruby Book 3", "Description of book 3")
 
+# Create user
+user_1 = User.new
+
+# Add books to library
 lib.add_book(book_1)
 lib.add_book(book_2)
 lib.add_book(book_3)
 
-lib.to_s
+# Check out book
+lib.check_out(book_1, user_1)
+
+# Test
+user_1.to_s
