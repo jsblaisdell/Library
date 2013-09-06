8da7f92ce9f4d4547c43d85b967e8e65405a61f7
Justin Blaisdell <jsblaisdell@gmail.com>
2013-09-06 17:49:00 -0500

    See which user has checked out a book.

--- library.rb
+++ library.rb
@@ -23,15 +23,26 @@ class Library
 end
 
 class Book
-  attr_accessor :author, :title, :description, :due_date
+  attr_accessor :author, :title, :description, :user, :due_date
   def initialize(author, title, description)
     @author = author
     @title = title
     @description = description
   end
+  def to_s
+    puts "\"#{@title}\" by #{@author}: #{@description}"
+    if @user == nil
+      puts "This book is available."
+    else
+      puts "#{@user} has checked this book out."
+    end
+  end
 end
 
 class User
+  def initialize(name)
+    @name = name
+  end
   @@checked_out_books = {}
 
   # See the books currently checked out.
@@ -57,6 +68,7 @@ class User
     if @@checked_out_books.length <= 2 && !overdue_books?
       @@checked_out_books[book.title] = book
       @@checked_out_books[book.title].due_date = Time.now + 604800
+      book.user = @name
       puts "Checked out #{book.title}. It's due #{@@checked_out_books[book.title].due_date.asctime}"
     elsif overdue_books?
       puts "You have overdue books. Please return them before checking out anything else."
@@ -73,12 +85,12 @@ end
 lib = Library.new
 
 # Create books
-book_1 = Book.new("First Author", "Ruby Book 1", "Description of book 1")
-book_2 = Book.new("Second Author", "Ruby Book 2", "Description of book 2")
-book_3 = Book.new("Third Author", "Ruby Book 3", "Description of book 3")
+book_1 = Book.new("First Author", "Ruby Book 1", "Description of book 1.")
+book_2 = Book.new("Second Author", "Ruby Book 2", "Description of book 2.")
+book_3 = Book.new("Third Author", "Ruby Book 3", "Description of book 3.")
 
 # Create user
-user_1 = User.new
+user_1 = User.new("User 1")
 
 # Add books to library
 lib.add_book(book_1)
@@ -88,9 +100,8 @@ lib.add_book(book_3)
 # Check out book
 lib.check_out(book_1, user_1)
 lib.check_out(book_2, user_1)
+puts
 
-puts
-user_1.to_s
-puts
-puts user_1.overdue_books?
-puts
+book_1.to_s
+book_2.to_s
+book_3.to_s
